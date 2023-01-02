8e4a3671bee8e1bd5645c4f3b26bcfbf6fb0d134
= <=>
2023-01-02 08:57:10 +0100

    sliding window

---
+++ slidingWindow.py
@@ -0,0 +1,20 @@
+arr = [80,-50,90,100]
+k = 2
+
+def maxSum(arr,windowSize):
+    arraySize = len(arr)
+    if(arraySize <= windowSize):
+        print("invalid operation")
+        return -1
+    
+    window_sum = sum([arr[i] for i in range(windowSize)])
+    max_sum = window_sum
+
+    for i in range(arraySize-windowSize):
+        window_sum = window_sum - arr[i] + arr[i+windowSize]
+        max_sum = max(window_sum, max_sum)
+    
+    return max_sum
+
+answer = maxSum(arr, k)
+print(answer)
